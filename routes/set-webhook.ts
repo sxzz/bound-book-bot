fc01ba022aaec71efc0e32874b24ca1ad5536591
三咲智子 Kevin Deng <sxzz@sxzz.moe>
2024-11-22 23:02:41 +0800

    refactor: pretty result

--- routes/set-webhook.ts
+++ routes/set-webhook.ts
@@ -4,12 +4,12 @@ import { bot, commands } from '../src'
 export default eventHandler(async (evt) => {
   const host = getRequestHeader(evt, 'x-forwarded-host') || getRequestHost(evt)
   const webhookUrl = `https://${host}/telegram-hook?token=${TOKEN}`
-  const isSet = await bot.telegram.setWebhook(webhookUrl)
+  const success = await bot.telegram.setWebhook(webhookUrl)
   const info = await bot.telegram.getWebhookInfo()
   await bot.telegram.setMyCommands(commands)
 
   return `Set webhook to ${webhookUrl.replaceAll(
     TOKEN,
     '*',
-  )}: ${isSet}<br/>${JSON.stringify(info).replaceAll(TOKEN, '*')}`
+  )}: ${success}<br/><pre>${JSON.stringify(info, undefined, 2).replaceAll(TOKEN, '*')}</pre>`
 })
