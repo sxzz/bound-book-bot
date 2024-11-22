c89933aaf4f6c786026353179222dcde570b6f63
三咲智子 Kevin Deng <sxzz@sxzz.moe>
2024-11-22 23:57:22 +0800

    feat: delete msg from url

--- routes/delete-msg.ts
+++ routes/delete-msg.ts
@@ -3,10 +3,22 @@ import { bot } from '../src'
 export default eventHandler(async (evt) => {
   const query = getQuery(evt)
 
-  const chatId = query.chat_id
-  if (typeof chatId !== 'string') return 'invalid chat_id'
-  if (typeof query.msg_id !== 'string') return 'invalid msg_id'
-  const msgId = +query.msg_id
+  const url = query.url
+  let chatId: string
+  let msgId: number
+
+  if (typeof url === 'string') {
+    // https://t.me/c/[chatId]/[msgId]
+    const urlObj = new URL(url)
+    const paths = urlObj.pathname.split('/')
+    chatId = `-100${paths[2]}`
+    msgId = +paths[3]
+  } else {
+    if (typeof query.chat_id !== 'string') return 'invalid chat_id'
+    chatId = query.chat_id
+    if (typeof query.msg_id !== 'string') return 'invalid msg_id'
+    msgId = +query.msg_id
+  }
 
   try {
     return await bot.telegram.deleteMessage(chatId, msgId)
